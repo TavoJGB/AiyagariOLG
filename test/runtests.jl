f567e94ed56e88b71fce9ecd45f5b3615c44fab9
TavoJGB <116122159+TavoJGB@users.noreply.github.com>
2025-06-01 21:16:00 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,13 +18,13 @@ tol = 1e-6
 # Test that we keep getting the same results for a given set of parameters
 model_tst = build_model("../Simulations/parameters/test_parameters.csv"; save_pars=false);
 # Steady-state computations
-eco = steady(model_tst...; r_0=0.04);
+eco_tst = steady(model_tst...; r_0=0.04);
 # Testing
 @testset "BENCHMARK SIMULATION: Steady State" begin
-    ss_test(eco; tol)
-    quantiles_test(eco; tol)
-    annualise!(eco)
-    @test compare_results(eco; nq=5, top=0.1) < tol
+    ss_test(eco_tst; tol)
+    quantiles_test(eco_tst; tol)
+    annualise!(eco_tst)
+    @test compare_results(eco_tst; nq=5, top=0.1) < tol
 end
 
 
@@ -32,9 +32,9 @@ end
 # Test that all equilibrium conditions are satisfied for the most recent simulation
 model_last = build_model("../Simulations/parameters/latest_simulation.csv"; save_pars=false);
 # Steady-state computations
-eco = steady(model_last...; r_0=0.04);
+eco_last = steady(model_last...; r_0=0.04);
 # Testing
 @testset "LATEST SIMULATION: Steady State" begin
-    ss_test(eco; tol)
-    quantiles_test(eco; tol)
+    ss_test(eco_last; tol)
+    quantiles_test(eco_last; tol)
 end
